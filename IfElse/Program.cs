cf559fddd26c83b58c0c64b9f8b79e6f3a2dbf3d
Valeriia <borisova.valeriya.n@mail.ru>
2023-10-17 20:55:43 +0300

    add condition

---
+++ IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "valeriia")
+{
+    Console.WriteLine("Hello sweety Valeriia!");
+}
+else
+{
+    Console.Write("Hello, nigga ");
+    Console.WriteLine(username);
+}
